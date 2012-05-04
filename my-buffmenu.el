f1d8341a6186dfa5c22454f1a81ff2a01c4d931f
Tim Woods <tim@thewoods.info>
2012-05-03 20:29:17 -0400

    Describe my-buffer-menu.
    Document what the my-buffer-menu does with and without prefix.  Remove
    the unnecessary message.

--- my-buffmenu.el
+++ my-buffmenu.el
@@ -1,8 +1,9 @@
 (provide 'my-buffmenu)
 
 (defun my-buffer-menu (arg)
+  "Show buffer menu with only files by default.  With prefix,
+show all buffers."
   (interactive "P")
-  (message "%s" arg)
   (if (eq arg 'nil)
       (buffer-menu 't)
     (buffer-menu)))
